a4dd114b0e957db3d9f57a9ac05cc6ea2f6c6dc8
John Marcial <johnmarloumarcial@gmail.com>
2022-06-09 03:25:53 +0800

    Initial Configuration push

---
+++ main.tf
@@ -0,0 +1,33 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
---
+++ variables.tf
@@ -0,0 +1,32 @@
+#variable "prefix" {
+#  type    = string
+#  #default = "sl-demo"
+#}
+
+#variable "resource_group_name" {
+#  type = string
+#}
+
+variable "location" {
+  type = string
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "Enter Subscription ID for provisioning resources in Azure"
+}
+
+variable "client_id" {
+  type        = string
+  description = "Enter Client ID for Application created in Azure AD"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Enter Client Secret for Application in Azure AD"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Enter Tenant ID of your Azure AD"
+}
