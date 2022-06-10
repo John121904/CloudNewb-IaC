d4fe6238f4a8a6fd72c94454097c9faafc28317c
John Marcial <johnmarloumarcial@gmail.com>
2022-06-11 03:18:44 +0800

    Updated OS & Data disks auto removal when destroying a VM

--- main.tf
+++ main.tf
@@ -69,10 +69,10 @@ resource "azurerm_virtual_machine" "main" {
   vm_size               = "Standard_A1_v2"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
+  delete_os_disk_on_termination = true
 
   # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
+  delete_data_disks_on_termination = true
 
   storage_image_reference {
     publisher = "Canonical"
