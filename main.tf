d753cf27bbde5da058aa3d2a0c7cb7a51abf8c84
John Marcial <johnmarloumarcial@gmail.com>
2022-06-10 08:48:37 +0800

    Added naming module and updated all resource names

--- main.tf
+++ main.tf
@@ -18,15 +18,25 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
 }
 
-# Create a resource group
+# # Create a resource group
+# resource "azurerm_resource_group" "example" {
+#   name     = "${var.prefix}-resources"
+#   location = "${var.location}"
+# }
+
+module "naming" {
+  source = "Azure/naming/azurerm"
+  prefix = ["cloudnewb"]
+}
 resource "azurerm_resource_group" "example" {
-  name     = "${var.prefix}-resources"
-  location = "${var.location}"
+  name     = module.naming.resource_group.name
+  location = var.location
 }
 
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "example" {
-  name                = "${var.prefix}-network"
+  #name                = "${var.prefix}-network"
+  name                = module.naming.virtual_network.name
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   address_space       = ["10.0.0.0/16"]
@@ -34,7 +44,8 @@ resource "azurerm_virtual_network" "example" {
 
 # Create a subnet
 resource "azurerm_subnet" "internal" {
-  name                 = "internal"
+  #name                 = "internal"
+  name                 = module.naming.subnet.name
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
@@ -42,7 +53,8 @@ resource "azurerm_subnet" "internal" {
 
 # Create a NIC
 resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
+  #name                = "${var.prefix}-nic"
+  name                = module.naming.network_interface.name
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
@@ -55,7 +67,8 @@ resource "azurerm_network_interface" "main" {
 
 # Create a VM
 resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
+  #name                  = "${var.prefix}-vm"
+  name                  = module.naming.linux_virtual_machine.name
   location              = azurerm_resource_group.example.location
   resource_group_name   = azurerm_resource_group.example.name
   network_interface_ids = [azurerm_network_interface.main.id]
@@ -94,7 +107,8 @@ resource "azurerm_virtual_machine" "main" {
 
 # Create App Service plan
 resource "azurerm_service_plan" "example" {
-  name                = "${var.prefix}-appserviceplan"
+  #name                = "${var.prefix}-appserviceplan"
+  name                = module.naming.app_service_plan.name
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   os_type             = "Linux"
@@ -103,7 +117,8 @@ resource "azurerm_service_plan" "example" {
 
 # Create App Service
 resource "azurerm_linux_web_app" "example" {
-  name                = "${var.prefix}-app-service"
+  #name                = "${var.prefix}-app-service"
+  name                = module.naming.app_service.name
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_service_plan.example.location
   service_plan_id     = azurerm_service_plan.example.id
