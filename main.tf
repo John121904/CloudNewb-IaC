8bf6656914eaccf156115483686003a3ebf4e320
John Marcial <johnmarloumarcial@gmail.com>
2022-06-10 09:22:30 +0800

    Added unique names for VM and Manage disk resources

--- main.tf
+++ main.tf
@@ -18,16 +18,14 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
 }
 
-# # Create a resource group
-# resource "azurerm_resource_group" "example" {
-#   name     = "${var.prefix}-resources"
-#   location = "${var.location}"
-# }
-
+# Create "naming" Module 
+# Ref: https://registry.terraform.io/modules/Azure/naming/azurerm/latest
 module "naming" {
   source = "Azure/naming/azurerm"
   prefix = ["cloudnewb"]
 }
+
+# Create a resource group
 resource "azurerm_resource_group" "example" {
   name     = module.naming.resource_group.name
   location = var.location
@@ -35,7 +33,6 @@ resource "azurerm_resource_group" "example" {
 
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "example" {
-  #name                = "${var.prefix}-network"
   name                = module.naming.virtual_network.name
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
@@ -44,7 +41,6 @@ resource "azurerm_virtual_network" "example" {
 
 # Create a subnet
 resource "azurerm_subnet" "internal" {
-  #name                 = "internal"
   name                 = module.naming.subnet.name
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
@@ -53,7 +49,6 @@ resource "azurerm_subnet" "internal" {
 
 # Create a NIC
 resource "azurerm_network_interface" "main" {
-  #name                = "${var.prefix}-nic"
   name                = module.naming.network_interface.name
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
@@ -67,8 +62,7 @@ resource "azurerm_network_interface" "main" {
 
 # Create a VM
 resource "azurerm_virtual_machine" "main" {
-  #name                  = "${var.prefix}-vm"
-  name                  = module.naming.linux_virtual_machine.name
+  name                  = module.naming.linux_virtual_machine.name_unique
   location              = azurerm_resource_group.example.location
   resource_group_name   = azurerm_resource_group.example.name
   network_interface_ids = [azurerm_network_interface.main.id]
@@ -87,7 +81,7 @@ resource "azurerm_virtual_machine" "main" {
     version   = "latest"
   }
   storage_os_disk {
-    name              = "myosdisk1"
+    name              = module.naming.managed_disk.name_unique
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
@@ -107,7 +101,6 @@ resource "azurerm_virtual_machine" "main" {
 
 # Create App Service plan
 resource "azurerm_service_plan" "example" {
-  #name                = "${var.prefix}-appserviceplan"
   name                = module.naming.app_service_plan.name
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
@@ -117,7 +110,6 @@ resource "azurerm_service_plan" "example" {
 
 # Create App Service
 resource "azurerm_linux_web_app" "example" {
-  #name                = "${var.prefix}-app-service"
   name                = module.naming.app_service.name
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_service_plan.example.location
